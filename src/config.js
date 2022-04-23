d71709bc47f24fcb5806e6c3296f95347f1a280e
Jose Acosta <jdap.dmc@gmail.com>
2022-04-23 03:41:39 -0400

    feat: template

--- src/config.js
+++ src/config.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 const config = {
 	// DATABASE
 
-	dbName: process.env.NAME_DB ?? 'login-jwt',
-	dbUser: process.env.USER_DB ?? 'root',
-	dbPassword: process.env.PASSWORD_DB ?? '123456', // inserte su password aqui
-	dbHost: process.env.HOST_DB ?? 'localhost',
+	dbName: process.env.NAME_DB || 'login-jwt',
+	dbUser: process.env.USER_DB || 'root',
+	dbPassword: process.env.PASSWORD_DB || '123456', // inserte su password aqui
+	dbHost: process.env.HOST_DB || 'localhost',
 
 	// SERVER
 
-	portServer: process.env.PORT_SERVER ?? 3001,
-	hostServer: process.env.HOST_SERVER ?? 'http://localhost'
+	portServer: process.env.PORT_SERVER || 3001,
+	hostServer: process.env.HOST_SERVER || 'http://localhost'
 };
 
 module.exports = config;
